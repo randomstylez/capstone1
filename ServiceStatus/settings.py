fbdb4b35bf515f7995e9a90d42acb4709eed583f
Arturo Quintana <arturocu81@gmail.com>
2020-03-25 11:05:19 -0400

    Some small modifications

--- ServiceStatus/settings.py
+++ ServiceStatus/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'status.apps.StatusConfig',
-    'django_extensions'
+    'django_extensions',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- status/models.py
+++ status/models.py
@@ -34,7 +34,8 @@ class Region(models.Model):
 
 class SubService(models.Model):
     sub_service_name = models.CharField(unique=True, max_length=100, verbose_name='Sub-Service')  # Field name made lowercase.
-    sub_service_description = models.CharField(max_length=100, blank=True, null=True)  # Field name made lowercase.
+    # sub_service_description = HTMLField()  # Field name made lowercase.
+    sub_service_description = models.TextField(blank=True, null=True)  # Field name made lowercase.
     services = models.ManyToManyField(Service, through='SubServiceServices', verbose_name='Service')
 
     class Meta:
