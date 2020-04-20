4d53ccb0aabfbc2a5968286b26cb6527e951edbe
Arturo Quintana <arturocu81@gmail.com>
2020-04-20 14:11:37 -0400

    minor changes - updated hostname to support the notification process

--- status/forms.py
+++ status/forms.py
@@ -401,7 +401,8 @@ class SubscriberForm(forms.ModelForm):
         email = user[0]['email']
         token = user[0]['token']
 
-        hostname = 'http://127.0.0.1:8000'
+        # hostname = 'http://127.0.0.1:8000'
+        hostname = 'http://service2.amlight.net'
 
         # we should create a mechanism to get the hostname. This option works on views request
         # print(HttpRequest.get_host(self))
@@ -444,7 +445,9 @@ class SubscriberForm(forms.ModelForm):
 
         token = str(user[0]['token'])  # Need to cast, otherwise it will have a nontype error
 
-        hostname = 'http://127.0.0.1:8000'
+        # hostname = 'http://127.0.0.1:8000'
+        hostname = 'http://service2.amlight.net'
+
         # we should create a mechanism to get the hostname. This option works on views request
         # print(HttpRequest.get_host(self))
 
