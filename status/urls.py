241290c37f0988c0e3314fc7f5e25059981328c7
Arturo Quintana <arturocu81@gmail.com>
2020-04-27 09:17:11 -0400

    minor changes Descriptive argument naming

--- status/urls.py
+++ status/urls.py
@@ -11,9 +11,9 @@ from .views import (
 urlpatterns = [
     path('', ServicesStatusView.as_view(), name='services_status_view'),
     path('subscription/', SubscriptionView.as_view(), name='subscription_view'),
-    path('<int:id>/', ServiceHistoryView.as_view(), name='service_history_view'),
-    path('subscription/<int:id>/', SubscriptionView.as_view(), name='subscription_view'),
-    path('details/<int:id>/', ServiceHistoryDetailsView.as_view(), name='service_history_details_view'),
+    path('<int:service_id>/', ServiceHistoryView.as_view(), name='service_history_view'),
+    path('subscription/<int:service_id>/', SubscriptionView.as_view(), name='subscription_view'),
+    path('details/<int:ticket_id>/', ServiceHistoryDetailsView.as_view(), name='service_history_details_view'),
     path('subscriber/<email>/<token>', ModifyUserSubscription.as_view(), name='modify_user_subscription_view')
 
 ]
