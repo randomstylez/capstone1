51e5b69b7379281bea553bb788e5ed18aaa216dc
Arturo Quintana <arturocu81@gmail.com>
2020-04-28 11:54:06 -0400

    minor changes - Modified naming StatusCategory model/object by Status - category_list by status_list - instance by status

--- status/admin.py
+++ status/admin.py
@@ -140,12 +140,12 @@ class TicketAdmin(admin.ModelAdmin):
         # If it is received data related to the ticket's logs, the ticket
         # will update its status with the last status registered on the logs
         if formset.cleaned_data:
-            status_category = formset.cleaned_data[-1]['status']
+            status = formset.cleaned_data[-1]['status']
             # If the last Ticket Log status is 'No Issues,' means that the problem has
             # updating the Ticket End Time to the value specified on the last Ticket Log
-            if status_category.status_category_tag == 'No Issues':
+            if status.status_category_tag == 'No Issues':
                 form.instance.end = formset.cleaned_data[-1]['action_date']
-            status_category_id = Status.objects.get(status_category_tag=status_category)
+            status_category_id = Status.objects.get(status_category_tag=status)
             form.instance.category_status_id = status_category_id.pk
             form.instance.save()
         formset.save()
