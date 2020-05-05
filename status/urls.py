e679293e5a7dce88686e04c1751239e1c4dfe90d
Arturo Quintana <arturocu81@gmail.com>
2020-05-05 15:23:21 -0400

    minor changes  - Applied pylint recommendations

--- status/urls.py
+++ status/urls.py
@@ -9,11 +9,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', ServicesStatusView.as_view(), name='services_status_view'),
-    path('subscription/', SubscriptionView.as_view(), name='subscription_view'),
-    path('<int:service_id>/', ServiceHistoryView.as_view(), name='service_history_view'),
-    path('subscription/<int:service_id>/', SubscriptionView.as_view(), name='subscription_view'),
-    path('details/<int:ticket_id>/', ServiceHistoryDetailsView.as_view(), name='service_history_details_view'),
-    path('subscriber/<email>/<token>', ModifyUserSubscription.as_view(), name='modify_user_subscription_view')
-
+    path('', ServicesStatusView.as_view(),
+         name='services_status_view'),
+    path('subscription/', SubscriptionView.as_view(),
+         name='subscription_view'),
+    path('<int:service_id>/', ServiceHistoryView.as_view(),
+         name='service_history_view'),
+    path('subscription/<int:service_id>/', SubscriptionView.as_view(),
+         name='subscription_view'),
+    path('details/<int:ticket_id>/', ServiceHistoryDetailsView.as_view(),
+         name='service_history_details_view'),
+    path('subscriber/<email>/<token>', ModifyUserSubscription.as_view(),
+         name='modify_user_subscription_view'),
 ]
